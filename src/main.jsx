da66229e1c98d37dc158841e7b50e04669635957
rafaelmsantos28 <rafaelmedeirostrab@gmail.com>
2025-06-17 00:49:11 -0300

    update: node modules and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/academia-do-arquiteto-de-software/">
+
       <App />
-    </BrowserRouter>
+
   </StrictMode>
 );
