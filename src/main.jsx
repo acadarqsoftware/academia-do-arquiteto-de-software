fae4e62a88ddafaa9511053bdfe6b10ba3198050
rafaelmsantos28 <rafaelmedeirostrab@gmail.com>
2025-06-16 23:11:17 -0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './style/index.css';
 import App from './App.jsx';
 
-// Aqui é onde será renderizado o componente principal (App.jsx) no index.html
-// Veja que ele pega o Id root lá do index.html para rederizar os componentes na página
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/academia-do-arquiteto-de-software/">
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
